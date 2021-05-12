bb8d43107b8990d4f0e343df507731187d8acf6e
Fran2333 <00161218@uca.edu.sv>
2021-05-11 19:31:46 -0600

    ejercicios labo 3

---
+++ tarea_laboratorio_3/ejercicio1.asm
@@ -0,0 +1,33 @@
+org 100h
+
+section .text
+
+        XOR AX, AX
+        XOR BX,BX;
+        XOR SI, SI
+        MOV BL, 8D
+        MOV word CX, 8D
+        MOV SI, 00
+        MOV byte [0200H], 0
+        MOV byte [0201H], 0
+        MOV byte [0202H], 1
+        MOV byte [0203H], 6
+        MOV byte [0204H], 1
+        MOV byte [0205H], 2
+        MOV byte [0206H], 1
+        MOV byte [0207H], 8
+
+        jmp iterar
+
+iterar:
+        ADD AL, [0200H + SI]
+        INC SI
+        LOOP iterar
+
+dividir:
+        DIV BL
+        MOV [020AH], AL
+        jmp exit
+
+exit:
+        int 20h
