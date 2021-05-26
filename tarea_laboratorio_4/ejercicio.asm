9b44441a8bea9c38daaa07d6b196e4cff5c2ff12
Fran2333 <00161218@uca.edu.sv>
2021-05-25 21:35:45 -0600

    ejercicio labo

---
+++ tarea_laboratorio_4/ejercicio.asm
@@ -0,0 +1,55 @@
+org 100h
+
+org     100h
+
+        section .text
+
+        XOR SI, SI                           
+        XOR DI, DI
+        XOR BX, BX
+        XOR CX, CX
+        XOR DX, DX
+
+        MOV     BP, arrByte     
+        CALL    StoreArr        
+
+        int     20h
+
+        section .data
+
+arrByte db      01,02,03,04,05,06,07,08,09,07     
+divs equ        02
+
+StoreArr:
+        MOV     BL, divs       
+iterar:  
+        CMP     SI,10
+        JE      end
+
+        XOR     AX, AX 
+        MOV     AL, [BP+SI]
+        MOV     BH,AL
+        DIV     BL
+        INC     SI
+
+        CMP     AH, 0
+        JE      par
+        JNE     impar
+        
+
+par:
+        MOV DI, CX    
+        MOV     [300h+ DI], BH
+        INC     DI
+        MOV     CX, DI   
+        jmp     iterar
+
+impar:
+        MOV DI, CX    
+        MOV     [320h+DI], BH
+        INC     DI
+        MOV     DX, DI   
+        jmp     iterar
+
+end:    
+        ret             
